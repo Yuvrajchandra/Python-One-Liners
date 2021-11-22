a0b882e74bf5462275d03ed1cc8f23f3665b274c
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-11-22 14:47:03 +0530

    check palindrome

---
+++ checkPalindrome.py
@@ -0,0 +1,8 @@
+str1 = "MUO"
+str2 = "madam"
+str3 = "MAKEUSEOF"
+str4 = "mom"
+print('Yes') if str1 == str1[::-1] else print('No')
+print('Yes') if str2 == str2[::-1] else print('No')
+print('Yes') if str3 == str3[::-1] else print('No')
+print('Yes') if str4 == str4[::-1] else print('No')
