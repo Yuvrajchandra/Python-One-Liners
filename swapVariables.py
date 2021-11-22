778cd904b4a930f9345fc6fd941fc94e1ccb7951
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-11-22 13:41:39 +0530

    swap two variables

---
+++ swapVariables.py
@@ -0,0 +1,7 @@
+a = 100
+b = 12
+print("Value of a before swapping:", a)
+print("Value of b before swapping:", b)
+a, b = b, a
+print("Value of a after swapping:", a)
+print("Value of b after swapping:", b)
