e0be26d3ad05e76594d99bd97e3b0b9e0e15b0cd
YuvrajChandra <Singhyuvraj179@gmail.com>
2021-11-24 02:02:37 +0530

    sort a list

---
+++ sortList.py
@@ -0,0 +1,14 @@
+list1 = [12, 345, 123, 34, 23, 37]
+list2 = ['m', 'a', 'k', 'e', 'u', 's', 'e', 'o', 'f']
+list3 = [5, 4, 3, 2, 1]
+print("Before Sorting:")
+print(list1)
+print(list2)
+print(list3)
+list1.sort()
+list2.sort()
+list3.sort()
+print("After Sorting:")
+print(list1)
+print(list2)
+print(list3)
